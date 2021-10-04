3ba4842a43d6d409650af6f441df82642da1f3bd
[Tom.Cockerill] <tomc37@hotmail.co.uk>
2021-10-04 10:03:09 +0100

    Correcting star sign names

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const signs = ['Cancer', 'Aries', 'Taurus', 'Gemini', 'Leo', 'Virgo', 'Libra', 'Scorpius', 'Sagittarius', 'Capricornus', 'Aquarius', 'Pisces'];
+const signs = ['Cancer', 'Aries', 'Taurus', 'Gemini', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'];
 const messages = ['You will be lucky in love today!', 'Today shall bring you wealth!', 'You will die today, sorry :(', 'Beware strange men in brown coats!', 'It is alien invasion day!'];
 const randSign = signs[Math.floor(Math.random()*signs.length)];
 const randMessage = messages[Math.floor(Math.random()*messages.length)];
 console.log ('Your sign is: ' + randSign);
-console.log('What this means for you: ' + randMessage);
+console.log('What this means for you: ' + randMessage);git status
