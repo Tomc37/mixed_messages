ca0c8752919006d148beec0cadf2c05a55c75453
[Tom.Cockerill] <tomc37@hotmail.co.uk>
2021-10-04 09:57:07 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+#Mixed Messages
+`A random message generator`
+
+##Table of Contents
+* [General info](#general-info)
+* [Technologies](#technologies)
+* [Setup](#setup)
+
+##General Info
+This project is a simple random message generator. Running the program will generate you a random star sign from the list, followed by another randomly selected prediction for your day.
+
+##Technologies
+This program was created entirely with JavaScript.
+
+##Setup
+In order to run the program, download it and open it with node.
---
+++ script.js
@@ -0,0 +1,6 @@
+const signs = ['Cancer', 'Aries', 'Taurus', 'Gemini', 'Leo', 'Virgo', 'Libra', 'Scorpius', 'Sagittarius', 'Capricornus', 'Aquarius', 'Pisces'];
+const messages = ['You will be lucky in love today!', 'Today shall bring you wealth!', 'You will die today, sorry :(', 'Beware strange men in brown coats!', 'It is alien invasion day!'];
+const randSign = signs[Math.floor(Math.random()*signs.length)];
+const randMessage = messages[Math.floor(Math.random()*messages.length)];
+console.log ('Your sign is: ' + randSign);
+console.log('What this means for you: ' + randMessage);
